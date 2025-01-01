3a13cd88427c343770ce144e6f66e033dcfa8767
Valentin Crogiez <crogiez.valentin@gmail.com>
2025-01-01 19:24:36 +0100

    Added an API custom link generator

---
+++ public/script.js
@@ -0,0 +1,70 @@
+const courseGroup = { 'TEC': 6, 'Projet': 6, 'RSX2': 6, 'GL': 4, 'JSFS': 5, 'Logique': 2, 'LAAS': 3, 'ARCHI': 3, 'PDS': 4, 'Découverte': 6 };
+const optionChoices = ['BIOINFO', 'PP', 'II2D', 'META', 'MAL', 'PDM'];
+const BASE_URL = 'https://calendar-exporter-api.vercel.app/generate?'
+
+const generateCourseDropdowns = (courseGroup) => {
+    const container = document.querySelector('.choice');
+    Object.entries(courseGroup).forEach(([course, maxOptions]) => {
+        const label = document.createElement('label');
+        label.textContent = course + ': ';
+        label.value = course;
+        const select = document.createElement('select');
+
+        const noneOption = document.createElement('option');
+        noneOption.value = 'none';
+        noneOption.textContent = 'none';
+        select.appendChild(noneOption);
+
+        if (course === 'Découverte') {
+            optionChoices.forEach(choice => {
+                const opt = document.createElement('option');
+                opt.value = choice;
+                opt.textContent = choice;
+                select.appendChild(opt);
+            })
+        } else {
+            for (let i = 1; i <= maxOptions; i++) {
+                const option = document.createElement('option');
+                option.value = i;
+                option.textContent = 'G' + i;
+                select.appendChild(option);
+            }
+        }
+
+        select.addEventListener('change', updateQueryString);
+
+        label.appendChild(select);
+        container.appendChild(label);
+    })
+}
+
+const generateQueryString = () => {
+    const selects = document.querySelectorAll('.choice select');
+    const params = [];
+
+    selects.forEach(select => {
+        const course = select.labels[0].value;
+        const value = select.value;
+        if (value !== 'none') {
+            params.push(`${course === 'Découverte' ? 'optionChoice' : course}=${value}`);
+        }
+    });
+
+    return params.join('&');
+}
+
+const updateQueryString = () => {
+    const textArea = document.getElementById("result");
+    params = generateQueryString();
+    textArea.value = BASE_URL + params;
+}
+
+const copyTextArea = () => {
+    var copyText = document.getElementById("result");
+    navigator.clipboard.writeText(copyText.value);
+    alert("ICS generation link has been copied to clipboard");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    generateCourseDropdowns(courseGroup);
+});
